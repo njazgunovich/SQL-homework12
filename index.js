c6ee634969287ba1a8e1ca7e7f6fe9d0f61bfec4
njazgunovich <nicholasjaz@gmail.com>
2021-02-10 15:35:00 -0500

    added functions and and questions

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 const mysql = require("mysql")
 const inquirer = require("inquirer")
-    // create a connection to the sql database
+require('dotenv').config();
+// create a connection to the sql database
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: "root",
+    user: "oldauth",
     password: process.env.DB_PASSWORD,
-    database: "homework12"
+    database: "homework12",
+
 })
 connection.connect((err) => {
     if (err) throw err;
     console.log("connected")
+    inquirer.prompt([{
+        name: "dbAction",
+        type: "list",
+        message: "what do you want to do",
+        choices: [
+            "create department",
+            "create role",
+            "create employees",
+        ]
+
+    }]).then((answer) => {
+        console.log(answer)
+        switch (answer.dbAction) {
+            case "create department":
+                createDepartment()
+                break;
+
+        }
+    })
 })
+
+function createDepartment() {
+    inquirer.prompt([{
+        name: "departmentParams",
+        type: "input",
+        message: "enter department name"
+    }]).then((answer) => {
+        console.log(answer)
+    })
+}
